a4ce71651b8b660224387d05cda4cdb405ced7af
Viktor <Rickeygame23@gmail.com>
2022-04-07 02:16:51 +0300

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             {
                 breakpoint: 768,
                 settings: {
-                    dots: true,
+                    // dots: true,
                     arrow: false
                }
             }
